d68ca5c388ddf087025817298a9aab0ba6e70278
Brad Van Skyhawk <brad@vanskyhawk.com>
2020-06-25 00:14:06 -0600

    Updates

---
+++ precache-manifest.523052ce923ac8d23b5388fadf84123e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61ee4f762be7f4c033fd5f4b4edf0eb4",
+    "url": "/react-test/index.html"
+  },
+  {
+    "revision": "037495b23fb06d0da934",
+    "url": "/react-test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "652a599d1a99efccd345",
+    "url": "/react-test/static/js/2.a430f49c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-test/static/js/2.a430f49c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "037495b23fb06d0da934",
+    "url": "/react-test/static/js/main.1ffa9b18.chunk.js"
+  },
+  {
+    "revision": "13a753d30ea0791a1f31",
+    "url": "/react-test/static/js/runtime-main.6550e32d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
